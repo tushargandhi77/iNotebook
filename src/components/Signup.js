a2fa76efa5ab5328eadbe1c9b5f4afd33ee2d1a4
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2023-03-12 12:12:24 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,7 +10,7 @@ const Signup = (props) => {
         if(credentials.password===credentials.cpassword)
         {
             const {name,email,password} = credentials
-            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            const response = await fetch("http://0.0.0.0:5000/api/auth/createuser", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
