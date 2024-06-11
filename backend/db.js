2701243508f3981ccfd38249ed05c54befe4bcc1
Tushar Gandhi <gandhitushar509@gamail.com>
2024-06-11 14:02:59 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const mongoURI = "mongodb://127.0.0.1:27017/test"
-const mongoURI = "mongodb://127.0.0.1:27017/test?connectTimeoutMS=1000&authSource=otherdb"
+const mongoURI = "mongodb+srv://inotebook:inotebook12345@cluster0.6tynf2l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.set("strictQuery", false);
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
